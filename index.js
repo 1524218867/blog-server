c9fee5d067451a87a29eeec34e934dfd2b3b341c
Your Name <you@example.com>
2026-01-24 16:43:08 +0800

    提交

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const fs = require('fs')
 
 const app = express()
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
 
 // 确保 uploads 目录存在
 const uploadDir = path.join(__dirname, 'uploads')
